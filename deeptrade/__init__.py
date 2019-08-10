66b0bcdf60bd1fc7b4f6c19b1a6a67826859127c
Fabio Capela <capela625@gmail.com>
2019-08-11 00:15:17 +0200

    cleaning a bit stuff

--- deeptrade/__init__.py
+++ deeptrade/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+
 # configuration variables
 
 api_key = None
@@ -8,4 +9,5 @@ api_base = "http://206.81.21.48/"
 
 
 # API sentiment
-from stripe.sentiment import *
+from deeptrade.sentiment import *
+
--- deeptrade/sentiment.py
+++ deeptrade/sentiment.py
@@ -1,12 +1,12 @@
-import request
-import stripe
+import requests
+import deeptrade
 import pandas as pd
 
 class Sentiment():
 
     def __init__(self):
 
-        self.head = {'Authorization': "Token %s" %stripe.api_key}
+        self.head = {'Authorization': "Token %s" %deeptrade.api_key}
 
     def by_date(self,date,dataframe=False):
         """
@@ -18,7 +18,7 @@ class Sentiment():
         json or pandas dataframe with all the ticker of the day date and 
         their corresponding sentiment
         """
-        endpoint = stripe.api_base+"sentiment_get/"+date
+        endpoint = deeptrade.api_base+"sentiment_get/"+date
         g = requests.get(endpoint, headers=self.head).json()
         if dataframe:
             df = pd.DataFrame(g)
