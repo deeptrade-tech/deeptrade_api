cce5f3862efe432e5878e66c1c6567cf57983726
Fabio Capela <capela625@gmail.com>
2019-08-11 00:37:28 +0200

    changed version of setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deeptrade",
-    version="0.0.1",
+    version="0.0.2",
     author="Fabio C.",
     author_email="info@deeptrade.ch",
     description="A python library to communicate with the DeepTrade API",
